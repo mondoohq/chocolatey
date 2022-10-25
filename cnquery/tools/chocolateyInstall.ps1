ec66a45e785d465e74cae4f15ede49b2c594cbc3
Jay Mundrawala <jay@mondoo.com>
2022-10-25 13:55:46 -0500

    choco package auto updater
    Signed-off-by: Jay Mundrawala <jay@mondoo.com>

---
+++ cnquery/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$version  = '7.0.3'
+$url      = "https://releases.mondoo.com/cnquery/${version}/cnquery_${version}_windows_amd64.zip"
+$checksum = '886c778d8368d71de18efb60f3cb24032c475e8c3eaabdab197597e36ed1130f'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url64bit      = $url
+
+  checksum64    = $checksum
+  checksumType64= 'sha256' #default is checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+
---
+++ cnspec/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$version  = '7.0.3'
+$url      = "https://releases.mondoo.com/cnspec/${version}/cnspec_${version}_windows_amd64.zip"
+$checksum = 'ce0e976e0f073d7155e3f0d355816658f8691bba81ec8614e480cc05a96e6a20'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url64bit      = $url
+
+  checksum64    = $checksum
+  checksumType64= 'sha256' #default is checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+
